816eee8340860331af541aab975e0d57c891d409
kotmin <kotmin@users.noreply.github.com>
2023-08-29 20:33:45 +0200

    Do not use it. It formats whole vault!!! Looks like while oppening obsidian method on create is also called

--- main.js
+++ main.js
@@ -5,21 +5,45 @@ module.exports = class NoteFormatterPlugin extends Plugin {
     async onload() {
         console.log('NoteFormatterPlugin loaded');
 
-        this.registerEvent(this.app.vault.on('create', (file) => {
-            this.formatNewNote(file.path);
+
+	//this.previousNoteName = null;
+
+        this.registerEvent(this.app.workspace.on('file-open', (file) => {
+            this.previousNoteName = file?.name;
         }));
+
+
+        this.registerEvent(this.app.vault.on('create', (file) => {
+            this.formatNewNote(file.path,this.previousNoteName);
+        }));
+
+
+
+
+
+
+
     }
 
-    async formatNewNote(notePath) {
-        const noteName = this.app.vault.getNameFromPath(notePath);
+    async formatNewNote(notePath,previousNoteName) {
+	console.log('New note created and got function');
+	//console.log(this.app.valut.getName());
+	//this.app.vault.
+        //const noteName = this.app.vault.getNameFromPath(notePath);
         const rootNotePath = this.app.vault.getRoot().path;
-        const rootNoteName = this.app.vault.getNameFromPath(rootNotePath);
+        //const rootNoteName = this.app.vault.getNameFromPath(rootNotePath);
 
-        const content = `# ${noteName}\n\n[[${rootNoteName}]]\n\n---\n\n### End of note template\n`;
+        //const content = `# ${noteName}\n\n[[${rootNoteName}]]\n\n---\n\n`;
+	// previousNoteName = previousNoteName.replace(/\.[^.]+$/, '');
+	const content = `# Hello TEST\n [[${previousNoteName}]] \n `;
+
+	
 
         const note = this.app.vault.getAbstractFileByPath(notePath);
         if (note) {
+	    console.log('note obj not empty');
             await this.app.vault.modify(note, content);
+	    console.log('Note succesfully formated');
         }
     }
 };
